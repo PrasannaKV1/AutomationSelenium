6a283e1b3faf1d4066c7a469178803d2ca7fdd04
PrasannaKV1 <prasannapooja215@gmail.com>
2024-12-09 12:07:51 +0530

    this is the message

---
+++ MavenProject/src/test/java/Arraysum1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Arraysum1 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" enter the size of the array ");
+        
+
+    }
+}
