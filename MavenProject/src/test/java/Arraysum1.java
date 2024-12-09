f3365cea6aa6c36487b9c5c29bd6202183c5fc1a
PrasannaKV1 <prasannapooja215@gmail.com>
2024-12-09 13:07:23 +0530

    this is the commit message

--- MavenProject/src/test/java/Arraysum1.java
+++ MavenProject/src/test/java/Arraysum1.java
@@ -5,7 +5,21 @@ public class Arraysum1 {
 
         Scanner sc=new Scanner(System.in);
         System.out.println(" enter the size of the array ");
-        
+        int N=sc.nextInt();
+        int array[]=new int[N];
+
+        for (int i = 0; i < array.length;i++) {
+            array[i]= sc.nextInt();
+
+        }
+        int sum=0;
+        for(int z = 0; z <array.length ; z++) {
+            sum+=array[z];
+
+        }
+        System.out.println(sum);
+
+
 
     }
 }
